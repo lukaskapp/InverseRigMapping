401f20135246e408142c9295ae559fe0004ec719
Lukas Kapp <lukas.kapp@filmakademie.de>
2023-03-22 02:25:25 +0100

    add dynamic building of anim tensor; add normalisation

--- gpr_predict.py
+++ gpr_predict.py
@@ -7,6 +7,9 @@ import csv
 from importlib import reload
 import pathlib
 import os
+import torch.autograd.profiler as profiler
+
+
 import multiTask_gpr as gpr
 reload(gpr)
 
@@ -30,7 +33,6 @@ def predict_data(anim_path):
     train_x = gpr.build_train_x_tensor(jnt_file)
     train_y, train_y_dimension = gpr.build_train_y_tensor(rig_file)
 
-    #likelihood, model, x_trans_min, x_trans_max = gpr.gpr()
     model_file = pathlib.Path(os.path.normpath(os.path.dirname(os.path.realpath(__file__))), "trained_model.pt")
     state_dict = torch.load(model_file)
 
@@ -44,46 +46,45 @@ def predict_data(anim_path):
     likelihood.eval()
 
 
-
     # load anim data
     anim_dataset = pd.read_csv(anim_path, na_values='?', comment='\t', sep=',', skipinitialspace=True, header=[0])
 
     # get number of objs in first column and mult it with len of data entries
     anim_x_depth =  len(np.unique(anim_dataset.iloc[:, :1]))
     anim_data_frames = int(len(anim_dataset)/anim_x_depth)
+    anim_dimension = len(anim_dataset.iloc[:, 3:].values[0])
 
-    mean_depth = len(anim_dataset.iloc[:, 2:].values[0])
+    raw_anim_x = np.array(anim_dataset.iloc[:, 3:]).reshape(-1, anim_dimension)
 
+    # normalisation: -1.0 to 1.0
+    attr_list = anim_dataset.columns.values[3:].tolist()
+    normalise_index_list = [attr_list.index(attr) for attr in attr_list if not "rotMtx_" in attr]
 
-    anim_x_raw = np.array(anim_dataset.iloc[:, 2:]).reshape(anim_x_depth, -1, mean_depth)
-    anim_x1 = torch.from_numpy(anim_x_raw[0]).float()
-    anim_x2 = torch.from_numpy(anim_x_raw[1]).float()
-    anim_x3 = torch.from_numpy(anim_x_raw[2]).float()
-
-    anim_x = torch.cat((anim_x1, anim_x2, anim_x3), -1).float()
-    #anim_x = torch.from_numpy(np.array(anim_dataset.iloc[:, 2:]).reshape(anim_x_depth, -1)).float()
-    anim_x = anim_x.to(device)
-
-    anim_x_trans = anim_x[:, :3]
-    anim_x_rot = anim_x[:, 3:]
-
-    # normalize data to range -1.0 1.0
+    #anim_x_min, anim_x_max = -170.0, 35.0
+    anim_x_min, anim_x_max = -50.0, 200.0
+    #anim_x_min, anim_x_max = -50.0, 50.0
     new_min, new_max = -1.0, 1.0
 
-    #x_trans_min, x_trans_max = -40.0, 40.0
-    #anim_x_trans_norm = (anim_x_trans - x_trans_min) / (x_trans_max - x_trans_min) * (new_max - new_min) + new_min
+    normalised_anim_x = raw_anim_x
+    for entry_index, entry in enumerate(normalised_anim_x):
+        for value_index, value in enumerate(entry):
+            if value_index in normalise_index_list:
+                value_norm = (value - anim_x_min) / (anim_x_max - anim_x_min) * (new_max - new_min) + new_min
+                normalised_anim_x[entry_index][value_index] = value_norm
 
-    #x_rot_min, x_rot_max = anim_x_rot.min(), anim_x_rot.max()
-    #anim_x_rot_norm = (anim_x_rot - x_rot_min) / (x_rot_max - x_rot_min) * (new_max - new_min) + new_min
-
-
-    #anim_x_norm = torch.cat((anim_x_trans_norm, anim_x_rot), -1).reshape(-1, mean_depth)
-    anim_x_norm = anim_x
+    cleaned_anim_x = np.array([entry for row in normalised_anim_x for entry in row if str(entry) != "nan"]) # remove n/a entries from data
+    anim_x_norm = torch.from_numpy(cleaned_anim_x.reshape(anim_data_frames, -1)).half()
+    anim_x_norm = anim_x_norm.to(device)
 
 
     # get predict values from trained model
-    predict_y = likelihood(model(anim_x_norm))
-    predict_mean = predict_y.mean
+    #anim_x_norm = torch.randn(2, 210)
+    predict_mean = []
+    with torch.no_grad():
+        for tensor in anim_x_norm:
+            predict_y = likelihood(model(tensor.reshape(1, -1)))
+            predict_mean.append(predict_y.mean.reshape(-1).tolist())
+
 
     # get rig dataset used in training for building predict data
     rig_file = pathlib.PurePath(os.path.normpath(os.path.dirname(os.path.realpath(__file__))), "training_data/rig", "irm_rig_data.csv")
@@ -92,6 +93,7 @@ def predict_data(anim_path):
     rig_obj_names = np.unique(train_rig_df.iloc[:, 1]) # get names of rig objects for data mapping later
     rig_dimension_list = train_rig_df.filter(items=["dimension"]).values[:len(rig_obj_names)] # get dimension per rig object for data mapping
 
+
     predict_data = []
     for data in predict_mean:
         start_range = 0
@@ -99,7 +101,7 @@ def predict_data(anim_path):
         for i, dimension in enumerate(rig_dimension_list):
             # isolate predict values of single obj
             dimension = int(dimension)
-            predict_values = data[start_range:(start_range + dimension)].tolist()
+            predict_values = data[start_range:(start_range + dimension)]
             start_range += dimension
 
             # use train data structure as base and replace values with predicted values
@@ -127,5 +129,5 @@ def predict_data(anim_path):
 
 
 
-#if __name__=="__main__":
-#    predict_data(anim_path=pathlib.PurePath(os.path.normpath(os.path.dirname(os.path.realpath(__file__))), "anim_data", "anim_data_01.csv"))
+if __name__=="__main__":
+    predict_data(anim_path=pathlib.PurePath(os.path.normpath(os.path.dirname(os.path.realpath(__file__))), "anim_data", "irm_anim_data.csv"))
