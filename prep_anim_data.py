887ed2144ab3e4b903587cb9aa1b8889880b98c2
Lukas Kapp <lukas.kapp@filmakademie.de>
2023-03-22 02:23:57 +0100

    add dynamic building of anim data based on jnt train data

--- prep_anim_data.py
+++ prep_anim_data.py
@@ -20,6 +20,19 @@ def prep_data():
     print("JNT LIST: ", jnt_list)
 
 
+
+
+
+    jnt_file = rig_file = pathlib.PurePath(os.path.normpath(os.path.dirname(os.path.realpath(__file__))), "training_data/jnt", "irm_jnt_data.csv")
+    with open(jnt_file, "r") as f:
+        reader = csv.reader(f)
+        anim_header = next(reader, None) # get header
+        jnt_data = [row for row in reader if len(row) != 0]
+
+        jnt_names = list(set([entry[1] for entry in jnt_data])) # get unique jnt names
+        jnt_dimension_list = [entry[2] for entry in jnt_data[:len(jnt_names)]]
+                    
+
     anim_data = []
     current_frame = cmds.currentTime(q=1)
     frames = []
@@ -27,29 +40,34 @@ def prep_data():
         frames.extend(list(set(cmds.keyframe(jnt, q=1))))
     frames = list(set(frames))
 
-    for i, jnt in enumerate(jnt_list):
-        for frame in frames:
-            cmds.currentTime(frame)
-            jnt_mtx = pm.dt.TransformationMatrix(cmds.xform(jnt, m=1, q=1, os=1))
-            jnt_rot_mtx3 = [x for mtx in jnt_mtx.asRotateMatrix()[:-1] for x in mtx[:-1]]
-            jnt_trans = jnt_mtx.getTranslation("object")
 
-            jnt_data = [i, jnt]
-            #jnt_data.extend(jnt_trans)
-            jnt_data.extend(jnt_rot_mtx3)
-            #jnt_data.extend(jnt_scale)
-            anim_data.append(jnt_data)
+    for frame in frames:
+        for i, jnt in enumerate(jnt_list):
+            cmds.currentTime(frame)
+            frame_data = [i, jnt, jnt_dimension_list[i]]
+            frame_data.extend(["n/a" for i in range(len(anim_header)-3)])
+            
+            train_values = jnt_data[i][3:]
+            attr_list = [anim_header[train_values.index(value) + 3] for value in train_values if value != "n/a"]
+
+            for attr in attr_list:
+                if not "rotMtx_" in attr:
+                    # replace only used attr of ctrls in n/a list, rest stays at n/a
+                    frame_data[anim_header.index(attr)] = cmds.getAttr("{}.{}".format(jnt, attr))
+
+            rotation = [attr for attr in attr_list if "rotMtx_" in attr]
+            if rotation:
+                jnt_mtx = pm.dt.TransformationMatrix(cmds.xform(jnt, m=1, q=1, os=1))
+                jnt_rot_mtx3 = [x for mtx in jnt_mtx.asRotateMatrix()[:-1] for x in mtx[:-1]]
+
+                start_index = anim_header.index("rotMtx_00") # get index of first rotMtx entry in anim_header and start replacing rotMtx values from there
+                for mtx_index, rot_mtx in enumerate(jnt_rot_mtx3):
+                    frame_data[start_index + mtx_index] = rot_mtx
+
+            anim_data.append(frame_data)
     
     cmds.currentTime(current_frame)
     print("FRAMES: ", frames)
-
-
-    header = ["No.", "jointName",
-                                "rotMtx_00", "rotMtx_01", "rotMtx_02",
-                                "rotMtx_10", "rotMtx_11", "rotMtx_12",
-                                "rotMtx_20", "rotMtx_21", "rotMtx_22"]
-
-
     print("DATA: ", anim_data)
     file_name = "irm_anim_data.csv"
     fullpath = pathlib.PurePath(os.path.normpath(os.path.dirname(os.path.realpath(__file__))), "anim_data", file_name)
@@ -57,7 +75,7 @@ def prep_data():
 
     with open(fullpath, "w") as f:
         writer = csv.writer(f)
-        writer.writerow(header)
+        writer.writerow(anim_header)
         writer.writerows(anim_data)
 
     return fullpath.as_posix(), frames
