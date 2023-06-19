37c13ac31a9880c9591fba233c97000c32fe7ac1
Lukas Kapp <lukas.kapp@filmakademie.de>
2023-06-19 18:12:21 +0200

    add progress bar; adjust subprocess for it

--- train_model.py
+++ train_model.py
@@ -3,22 +3,39 @@ import sys
 import os
 import pathlib
 import maya.api.OpenMaya as om
+import maya.cmds as cmds
 
 
-def train_model(py_app, rig_path="irm_rig_data.csv", jnt_path="irm_jnt_data.csv", model_path="trained_model.pt", lr=0.01, epochs=100, force_cpu=False):
+def train_model(py_app, rig_path, jnt_path, model_path, lr=0.01, epochs=100, force_cpu=False):
     py_path = pathlib.Path(os.path.normpath(os.path.dirname(os.path.realpath(__file__))))
     py_cmd = f"import sys; sys.path.append('{py_path}'); import gpr_model as gpr; gpr.train_model('{rig_path}', '{jnt_path}', '{model_path}', {lr}, {epochs}, {force_cpu})"
-    
-    command = [py_app, "-c", py_cmd]
-    print(command)
+    command = [py_app, "-u", "-c", py_cmd]
 
-    process = subprocess.run(command, capture_output=True, text=True)
+    # Initialize the progress window
+    cmds.progressWindow(title='Training Model', progress=0, status='Initialize Training...', isInterruptable=True)
+
+    # start subprocess; prevent output window
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                                text=True, creationflags=subprocess.CREATE_NO_WINDOW)
+
+    # update progress bar
+    for line in iter(process.stdout.readline, ""):
+        if line.startswith("Iter "):
+            current_epoch = str(line.split("/")[0].partition("Iter ")[2])
+            current_loss = str(line.split("Loss: ")[1])
+
+        if line.startswith("PROGRESS "):
+            progress = float(line.strip().split(" ")[1])
+            if cmds.progressWindow(query=True, isCancelled=True):
+                process.kill()
+                break
+            cmds.progressWindow(edit=True, progress=progress, status=(f'Epochs: {current_epoch}/{epochs}   -   Loss: {current_loss}'))
+
+    process.wait()
+
+    cmds.progressWindow(endProgress=True)
 
     if process.returncode:
         raise ValueError(process.stderr)
     else:
-        print(process.stdout)
-        om.MGlobal.displayInfo("Model trained successfully!")
-
-if __name__ == "__main__":
-    train_model()
+        om.MGlobal.displayInfo(f"Model trained successfully in {epochs} epochs and end loss of {current_loss}")
