7baaa4d886f459e77dbb003e58d8ec85d064df46
Lukas Kapp <lukas.kapp@filmakademie.de>
2023-07-19 00:58:15 +0200

    add TD header

---
+++ utils/__init__.py
@@ -0,0 +1,10 @@
+"""
+-----------------------------------------------------------------------------
+This file has been developed within the scope of the
+Technical Director course at Filmakademie Baden-Wuerttemberg.
+http://technicaldirector.de
+
+Written by Lukas Kapp
+Copyright (c) 2023 Animationsinstitut of Filmakademie Baden-Wuerttemberg
+-----------------------------------------------------------------------------
+"""
